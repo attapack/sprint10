8264961b56e0df1ffa22eeb949b9bda8508ccc82
attapack <75312811+attapack@users.noreply.github.com>
2021-09-24 18:22:42 +0300

    Create header.h

---
+++ t01/inc/header.h
@@ -0,0 +1,16 @@
+  
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int mx_strlen(const char *s);
+void mx_printerr(const char *s);
+void mx_printstr(const char *s);
+int mx_strlen(const char *s);
+void mx_printchar(char c);
+
+#endif
